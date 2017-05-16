e3f87a1129b35203ee14d21366aef412ff8aae0a
David Shin and Jonathon Kang <david+jonathon@>
2017-05-16 11:08:21 -0700

    update the index view.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,14 +9,17 @@ var moves = {
 function endAct() {
   actionPhase = false;
   buyPhase = true;
+  $('#phaseText').text('Buy Phase');
+  $('#endAct').hide();
+  $('#endTurn').show();
 }
 
 
 $(document).ready(function(){
-  boardArr = $('#boardData').data('stuff')
+  boardArr = $('#boardData').data('board')
   console.log(boardArr)
   for(i=0;i<boardArr.length;i++){
-    $('#boardRow').append('<div class="col-md-2 cardDiv"><img class="card board_cards" src="/img/smithy.jpg"></div>')
+    $('#boardRow').append('<div class="col-md-2 cardDiv"><img class="card board_cards" src="'+boardArr[i].image+'"><p>Amount: '+boardArr[i].amount+'</p></div>')
   }
 
 
@@ -24,6 +27,7 @@ $(document).ready(function(){
   $('.hand_cards').click(function(){
     if(actionPhase&&moves['actions']>0){
       //do the thing
+
       moves['actions']--;
       $('#actText').text(moves['actions'])
     }
@@ -35,6 +39,7 @@ $(document).ready(function(){
   $('.board_cards').click(function(){
     if(buyPhase&&moves['buys']>0){
       //do the thing
+
       moves['buys']--;
       $('#buyText').text(moves['buys'])
     }
