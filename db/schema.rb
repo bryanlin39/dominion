25deec44c21edf7d9865b835cf0f62d735c184fb
Bryan Lin and Ilene Gorski <bryan+ilene@>
2017-05-16 10:24:34 -0700

    add ghost changes to schema so that we can pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515231039) do
+ActiveRecord::Schema.define(version: 20170516164659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170515231039) do
     t.integer "money_value"
     t.integer "victory_value"
     t.string "card_type"
+    t.string "image"
   end
 
   create_table "decks", force: :cascade do |t|
