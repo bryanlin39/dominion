7a3e03bd9917969fcbdb463e8df0ee56df1fb4bb
Bryan Lin and Ilene Gorski <bryan+ilene@>
2017-05-16 11:06:36 -0700

    add Deck.setup method

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,8 +4,22 @@ class Deck < ActiveRecord::Base
 
   before_create(:discard)
 
+  def self.setup
+    estate = Card.where(name: 'Estate')
+    copper = Card.where(name: 'Copper')
+    Player.all.each do |player|
+      3.times do
+        player.cards.push(estate)
+      end
+      7.times do
+        player.cards.push(copper)
+      end
+    end
+  end
+
   private
   def discard
     self.location = 'discard'
   end
+
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -7,4 +7,14 @@ describe Deck do
       expect(deck.location).to eq('discard')
     end
   end
+
+  describe '.setup' do
+    it "deals 3 estates and 7 coppers to each player" do
+      estate=Card.create({:name=>'Estate'})
+      copper=Card.create({:name=>'Copper'})
+      player = Player.create(name: 'Bryan')
+      Deck.setup
+      expect(player.cards.length).to eq(10)
+    end
+  end
 end
