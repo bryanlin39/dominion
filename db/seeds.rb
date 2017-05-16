ad60cebdf5e730818bafe54f1f2d33642514212b
David Shin and Jonathon Kang <david+jonathon@>
2017-05-16 10:37:19 -0700

    update the seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@ end
 class Supply < ActiveRecord::Base
   has_many :cards
 end
+class Player < ActiveRecord::Base
+  has_many :decks
+  has_many :cards, through: :decks
+end
 
 estate=Card.create({:name=>'Estate', :cost=>2, :rules=>'', :money_value=>0, :victory_value=>1, :card_type=>'victory', :image =>'/img/estate.jpg' })
 duchy=Card.create({:name=>'Duchy', :cost=>5, :rules=>'', :money_value=>0, :victory_value=>3, :card_type=>'victory', :image =>'/img/duchy.jpg' })
@@ -16,8 +20,8 @@ gold=Card.create({:name=>'Gold', :cost=>6, :rules=>'', :money_value=>3, :victory
 smithy=Card.create({:name=>'Smithy', :cost=>4, :rules=>'', :money_value=>0, :victory_value=>0, :card_type=>'action', :image =>'/img/smithy.jpg' })
 village=Card.create({:name=>'Village', :cost=>3, :rules=>'', :money_value=>0, :victory_value=>0, :card_type=>'action', :image =>'/img/village.jpg' })
 chapel=Card.create({:name=>'Chapel', :cost=>2, :rules=>'', :money_value=>0, :victory_value=>0, :card_type=>'action', :image =>'/img/chapel.jpg' })
-market=Card.create({:name=>'Market', :cost=>5, :rules=>'', :money_value=>1, :victory_value=>0, :card_type=>'action', :image =>'/img/market.jpg' })
-woodcutter=Card.create({:name=>'Woodcutter', :cost=>3, :rules=>'', :money_value=>2, :victory_value=>0, :card_type=>'action', :image =>'/img/woodcutter.jpg' })
+market=Card.create({:name=>'Market', :cost=>5, :rules=>'', :money_value=>0, :victory_value=>0, :card_type=>'action', :image =>'/img/market.jpg' })
+woodcutter=Card.create({:name=>'Woodcutter', :cost=>3, :rules=>'', :money_value=>0, :victory_value=>0, :card_type=>'action', :image =>'/img/woodcutter.jpg' })
 
 Supply.create({:card_id=>estate.id, :amount=>24})
 Supply.create({:card_id=>duchy.id, :amount=>12})
@@ -30,3 +34,8 @@ Supply.create({:card_id=>village.id, :amount=>10})
 Supply.create({:card_id=>chapel.id, :amount=>10})
 Supply.create({:card_id=>market.id, :amount=>10})
 Supply.create({:card_id=>woodcutter.id, :amount=>10})
+
+Player.create({:name=>'David', :victory_points=>0, :player_num=>3})
+Player.create({:name=>'Ilene', :victory_points=>0, :player_num=>1})
+Player.create({:name=>'Jon', :victory_points=>0, :player_num=>2})
+Player.create({:name=>'Bryan', :victory_points=>0, :player_num=>4})
